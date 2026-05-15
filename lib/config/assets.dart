95cd8551c0320f6440ffdf297ca546a5733c994e
Paresh <ritejrdev@gmail.com>
2026-05-16 01:54:30 +0530

    feat: Simplify logo handling by removing dynamic path logic and using a single logo asset

--- lib/config/assets.dart
+++ lib/config/assets.dart
@@ -1,30 +1,5 @@
 class AppAssets {
-  // Set this to 1, 2, or 3 to select branding set.
-  static const int activeSet = 1;
-
-  static String get fullLogoPath {
-    switch (activeSet) {
-      case 2:
-        return 'assets/images/sub3.png';
-      case 3:
-        return 'assets/images/sub5.png';
-      case 1:
-      default:
-        return 'assets/images/sub1.png';
-    }
-  }
-
-  static String get halfLogoPath {
-    switch (activeSet) {
-      case 2:
-        return 'assets/images/sub4.png';
-      case 3:
-        return 'assets/images/sub6.png';
-      case 1:
-      default:
-        return 'assets/images/sub2.png';
-    }
-  }
+  static const String logoPath = 'assets/images/logo.png';
 
   static const List<String> galleryImages = [
     'assets/images/sub1.png',
