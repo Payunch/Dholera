2df2d1d9e7a5ca569ff09875581f4780d0ef0684
Paresh Solanki <solankiparesh1183@gmail.com>
2026-08-06 18:52:31 +0530

    chore: flutter app architecture cleanup

---
+++ lib/services/firebase_secrets.dart
@@ -0,0 +1,4 @@
+class FirebaseSecrets {
+  static const String webApiKey = 'AIzaSyBN6qClTk28er9L_AoQnko6M8weNp4bLZk';
+  static const String androidApiKey = 'AIzaSyDCxPGN4lr4Kka0UYD0uNn0N853iLVtRAA';
+}
